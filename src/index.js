ebf85450f88d113a3a7837e4b0016dc4882d1de2
Arnoldo Sanchez <arnoldo1098@gmail.com>
2022-05-20 11:05:24 -0500

    Configuring React

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+/* The following line can be included in your src/index.js or App.js file*/
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
